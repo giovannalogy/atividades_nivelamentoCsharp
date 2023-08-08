114fc249dacd23ed6061c5c2574d334f84a2bd4a
giovannalogy <giovannadiasney@gmail.com>
2023-08-08 13:45:28 -0300

    Segunda atividade

---
+++ Atividade I/AtividadeII/Produto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace AtividadeII
+{
+    public class Produto
+    {
+        //Atributos privados da classe Produto
+        private string Nome { get; set; }
+        private double Preco { get; set; }
+        private int Quantidade { get; set; }
+
+        //Construtor da classe 
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+
+        }
+             
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade; 
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            if (quantidade <= Quantidade)
+            {
+                Quantidade -= quantidade;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade do estoque está induficiente.");
+            }
+        }
+
+        public void MostrarDados()
+        {
+            Console.WriteLine($"Nome: {Nome}\nPreço: {Preco:C}\nQuantidade em Estoque: {Quantidade}\nValor Total em Estoque: {ValorTotalEmEstoque():C}");
+        }
+    }
+}
---
+++ Atividade I/AtividadeII/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AtividadeII
+{
+    public class Estoque
+    {
+        static void Main()
+        {
+            Console.WriteLine("Insira o nome do produto");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Insira o preço do produto:");
+            double preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a quantidade do produto:");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto produto = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine("\nDados do Produto:");
+            produto.MostrarDados();
+
+            Console.WriteLine("\nRealizar entrada de estoque:");
+            int entrada = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(entrada);
+
+            Console.WriteLine("\nDados do produto após entrada:");
+            produto.MostrarDados();
+
+            Console.WriteLine("\nRealizar saída de estoque:");
+            int saida = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(saida);
+
+            Console.WriteLine("\nDados do produto após saída:");
+            produto.MostrarDados();
+
+        }
+    }
+}
